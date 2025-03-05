9b7f54337ef7282c7e8f4c18a032fc03ab737986
gulgen <gulgendemir@gmail.com>
2025-03-05 15:07:32 +0300

    addes some project

--- src/ArihtmeticOperations/ArithmeticOperations.java
+++ src/ArihtmeticOperations/ArithmeticOperations.java
@@ -9,10 +9,13 @@ public class ArithmeticOperations {
 
         System.out.println("Birinci sayıyı girin:");
         int a = scanner.nextInt();
+
         System.out.println("İkinci sayıyı girin:");
         int b = scanner.nextInt();
+
         System.out.println("Üçüncü sayıyı girin:");
         int c = scanner.nextInt();
+
         int sonuc = (a + b) * (c - b);
         System.out.println("Sonuç:" + sonuc);
 
